606a49179cac7a1bc87c0934c5439dbef758607b
Irina Inozemtseva <rishaleva@yandex.ru>
2024-02-18 18:38:43 +0300

    producer kafka created

---
+++ src/main/java/com/rishaleva/kafkaproducer/controller/ProducerController.java
@@ -0,0 +1,26 @@
+package com.example.kafka.controller;
+
+import com.example.kafka.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class ProducerController {
+
+    private final KafkaTemplate<String, User> kafkaRestTemplate;
+
+    private final String TOPIC = "user_topic";
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestBody User user) {
+        String key = UUID.randomUUID().toString();
+        kafkaRestTemplate.send(TOPIC, key, user);
+        return "Message sent successfully with key " + key + " and value " + user.toString();
+    }
+}
